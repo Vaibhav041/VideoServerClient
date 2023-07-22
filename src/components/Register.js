6570d348d22e8d1affe0ec710c98cc3fa2e92197
vaibhav <vaibhavsahni2001@gmail.com>
2023-07-22 17:14:45 +0530

    password

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { update } from '../redux/userSlice';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { update } from "../redux/userSlice";
 
 const Register = () => {
   const [user, setUser] = useState();
@@ -10,36 +10,52 @@ const Register = () => {
 
   const handleClick = async () => {
     console.log(user);
-    let data = await fetch('https://videostream-y3uo.onrender.com/register', {
-        method:'post',
-        body:JSON.stringify(user),
-        credentials: "include",
-        headers:{
-            'Content-Type':'application/json'
-        }
-    })
+    let data = await fetch("https://videostream-y3uo.onrender.com/register", {
+      method: "post",
+      body: JSON.stringify(user),
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     data = await data.json();
     if (data.error) {
       alert(data.error);
+    } else {
+      navigate("/login");
     }
-    else {
-      navigate('/login');
-    }
-}
+  };
   const handleChange = (e) => {
-    setUser({...user, [e.target.name]:e.target.value});
-  }
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
   return (
-    <div className='loginPage'>
-      <form className='loginForm'>
-        <h1 style={{color:"orange", textAlign:"center"}}>Register</h1>
-        <input type='text' placeholder='Username' name='name' onChange={handleChange}/>
-        <input type='text' placeholder='Email' name='email' onChange={handleChange}/>
-        <input type='text' placeholder='Password' name='password' onChange={handleChange}/>
-        <button className='btn btn-danger' type='button' onClick={handleClick}>Submit</button>
+    <div className="loginPage">
+      <form className="loginForm">
+        <h1 style={{ color: "orange", textAlign: "center" }}>Register</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          name="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button className="btn btn-danger" type="button" onClick={handleClick}>
+          Submit
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
